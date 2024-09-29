d4394c6347817c167c1b957a2fb680e8d189d950
misty <yessine305@gmail.com>
2024-09-28 21:49:04 -1200

    adding items with animation in here section

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -6,7 +6,7 @@ Files: hacker-room-new.glb [34.62MB] > /Users/hsuwinlat/Desktop/jsm pj/threejscc
 
 import { useGLTF, useTexture } from '@react-three/drei';
 
-const HackerRoom = (props) => {
+export function HackerRoom(props) {
   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
   const monitortxt = useTexture('textures/desk/monitor.png');
@@ -39,5 +39,3 @@ const HackerRoom = (props) => {
 }
 
 useGLTF.preload('/models/hacker-room.glb');
-
-export default HackerRoom;
